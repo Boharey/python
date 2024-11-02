c868360296c3f37c18c9ce37bb1b971b87319cb5
Boharey <utsavbohara4@gmail.com>
2024-11-02 16:36:21 +0545

    practiced len once again

--- general/built-in-functions/iterable-functions/len().py
+++ general/built-in-functions/iterable-functions/len().py
@@ -15,3 +15,16 @@ class Book():
 
 b1 = Book(pages = 300)
 print(len(b1))
+
+class Bike():
+  def __init__(self,cost: int,company: str = "unnamed")-> None:
+    self.company = company
+    self.price = cost # in thousands(K)
+  
+  def __len__(self) -> int:
+    return self.price
+  #using len() function now causes to return the cost if the bike 
+  
+bk1 = Bike(10)
+print(len(bk1)) # returns 10
+  
